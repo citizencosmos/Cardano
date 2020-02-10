d7491e3c44180e2b56400a0411fc35fcee0cabcd
citizen cosmos <59578785+citizencosmos@users.noreply.github.com>
2020-02-10 14:22:04 -0500

    Update wrapper_test.sh

--- wrapper_test.sh
+++ wrapper_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 read -p "Hey, good morning! What's your name?  " $username
+echo "Let's gather some preliminary information for the second script..."
+read -p "What's your favorite 2 or 3 digit number?  " $1
 
-#path of external script
+#path of external script, use full path ~/ didn't seem to work
 SCRIPT2="/home/citizen/2_digits_test.sh"
-#execute it
+
+#execute the external script
 . "$SCRIPT2"
+
 #back to our wrapper script
-echo "well, OK! that worked out very nicely"
+echo " OK, did the number turn out like you hoped it would "
+#did it reset the variable $username to your second response, gathered by the external script?
+echo "well, OK! that almost worked out very nicely " $username
