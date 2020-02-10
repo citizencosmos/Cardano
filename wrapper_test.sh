c04d6073011287e60bd683336c3e5e68c357e731
citizen cosmos <59578785+citizencosmos@users.noreply.github.com>
2020-02-10 10:03:45 -0500

    just testing script in a script

---
+++ wrapper_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Hey, good morning! What's your name?  " $username
+
+#path of external script
+SCRIPT2="~/2_digits_test.sh"
+#execute it
+. "$SCRIPT2"
+#back to our wrapper script
+echo "well, OK! that worked out very nicely"
