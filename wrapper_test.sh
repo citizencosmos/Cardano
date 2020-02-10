00fc3a995ba51c3fac73307482ba908a3b6507cc
citizen cosmos <59578785+citizencosmos@users.noreply.github.com>
2020-02-10 10:04:53 -0500

    Update wrapper_test.sh

--- wrapper_test.sh
+++ wrapper_test.sh
@@ -3,7 +3,7 @@
 read -p "Hey, good morning! What's your name?  " $username
 
 #path of external script
-SCRIPT2="~/2_digits_test.sh"
+SCRIPT2="/home/citizen/2_digits_test.sh"
 #execute it
 . "$SCRIPT2"
 #back to our wrapper script
